97a9c3673db80bcceab8f0bf662f3f24940db9cd
Luiz henrique <luizmon.silva@gmail.com>
2019-09-03 22:35:59 -0300

    buscando informação do video youtube

--- src/components/youtube/YoutubeDownloader.vue
+++ src/components/youtube/YoutubeDownloader.vue
@@ -22,9 +22,28 @@
               single-line
             />
           </v-flex>
+          <v-flex md12 xs12 v-if="showVideoInfo">
+            <v-card max-width="600">
+              <v-row class="py-4 pl-4">
+                <v-col>
+                  <v-card-text>{{videoInfo.player_response.videoDetails.title}}</v-card-text>
+                </v-col>
+                <v-col class="shrink">
+                  <v-img
+                    :height="videoInfo.player_response.videoDetails.thumbnail.thumbnails[0].height"
+                    :width="videoInfo.player_response.videoDetails.thumbnail.thumbnails[0].width"
+                    :src="videoInfo.player_response.videoDetails.thumbnail.thumbnails[0].url"
+                  />
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-flex>
           <v-flex xs12 md12>
             <v-btn color="primary" @click.stop="downloadVideo" :disabled="!valid">
-              <v-icon left>mdi-magnify</v-icon>Baixar
+              <v-icon left>mdi-magnify</v-icon>Download
+            </v-btn>&nbsp;
+            <v-btn color="primary" :disabled="!valid" @click.stop="getYoutubeVideoInfo">
+              <v-icon left>mdi-card-search-outline</v-icon>Information
             </v-btn>
           </v-flex>
         </v-layout>
@@ -42,6 +61,8 @@ export default {
     youtubeUrlRules: [v => v.length > 12 || "Url youtube invalida"],
     videoName: "",
     select: { state: "highest", descricao: "highest audio and video" },
+    videoInfo: {},
+    showVideoInfo: false,
     qualityItems: [
       { state: "highest", descricao: "highest audio and video" },
       { state: "lowest", descricao: "lowest audio and video" },
@@ -54,7 +75,18 @@ export default {
   methods: {
     downloadVideo() {
       const url = `http://localhost:5001/api/yt-download?yt_url=${this.ytUrl}&quality=${this.select.state}&name=${this.videoName}`;
-       window.open(url);
+      window.open(url);
+    },
+    getYoutubeVideoInfo() {
+      this.$multiservice
+        .get(`yt-info?yt_url=${this.ytUrl}`)
+        .then(resp => {
+          this.videoInfo = resp.data;
+
+          console.log(resp);
+          this.showVideoInfo = true;
+        })
+        .catch(err => alert(err));
     }
   }
 };
