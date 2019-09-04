f89c12073be2bbfc34966ae6be33b8e0be12e436
Luiz henrique <luizmon.silva@gmail.com>
2019-09-03 23:40:10 -0300

    add cdn carousel files on cloudinary

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,7 @@
   <v-layout wrap>
     <v-flex md12>
       <v-carousel>
-        <v-carousel-item v-for="(color, i) in colors" :key="color">
-          <v-sheet :color="color" height="100%" tile>
-            <v-row class="fill-height" align="center" justify="center">
-              <div class="display-3">Slide {{ i + 1 }}</div>
-            </v-row>
-          </v-sheet>
-        </v-carousel-item>
+        <v-carousel-item v-for="(image, i) in images" :key="i" :src="image"></v-carousel-item>
       </v-carousel>
     </v-flex>
   </v-layout>
@@ -17,7 +11,11 @@
 <script>
 export default {
   data: () => ({
-    colors: ["primary", "secondary", "yellow darken-2", "red", "orange"]
+    images: [
+      "https://res.cloudinary.com/luizhenrique/image/upload/v1567563719/carousel/mangas-carousel-image-1.png",
+      "https://res.cloudinary.com/luizhenrique/image/upload/v1567563719/carousel/youtube-carousel-image-1.png",
+      "http://res.cloudinary.com/luizhenrique/image/upload/v1567563719/carousel/youtube-carousel-image-2.png",
+    ]
   })
 };
 </script>
